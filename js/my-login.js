00fb680e163148eb08d17f5840e6e5ad912fdb08
jhonsalazar01 <93412378+jhonsalazar01@users.noreply.github.com>
2024-11-22 20:25:29 -0500

    Add files via upload

---
+++ js/my-login.js
@@ -0,0 +1,81 @@
+document.querySelector("#btnSend").addEventListener('click', () => {
+	const email = document.querySelector('#email').value; 
+	const password = document.querySelector('#password').value; 
+  
+	const data = { email: email, password: password };
+  
+	const URL = "http://localhost:4081/api/user/login";
+
+	errorMessage.style.display = 'none';
+
+	loader.style.display = 'flex';
+  
+	fetch(URL, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify(data)
+	})
+	.then(resp => resp.json())
+	.then(responseData => {
+		
+		if (responseData.message) {
+			loader.style.display = 'none';
+			
+			alert(responseData.message); // Muestra el mensaje de bienvenida
+			if (responseData.data && responseData.data.token) {
+				localStorage.setItem('authToken', responseData.data.token); // Almacena el token
+				if (responseData.data.userId) {
+					localStorage.setItem('userId', responseData.data.userId);
+				}
+
+				alert(`Token: ${responseData.data.token}\nUser ID: ${responseData.data.userId}`);
+				window.location.href = 'reserva.html'; // Redirige a la página protegida
+			}
+		} else {
+			loader.style.display = 'none';
+			errorMessage.style.display = 'flex';
+		//	alert("Error en el inicio de sesión. Intenta nuevamente."); // Manejo de errores
+		}
+	})
+	.catch(err => {
+		console.error(err);
+		loader.style.display = 'none';
+		errorMessage.style.display = 'flex';
+		alert("Ocurrió un error. Intenta nuevamente.");
+	});
+  
+
+});
+
+function closeErrorMessage() {
+    const errorMessage = document.getElementById('errorMessage');
+    errorMessage.classList.add('hide'); // Agrega la clase para iniciar la animación
+
+    // Después de un tiempo (300ms), oculta el elemento completamente
+    setTimeout(() => {
+        errorMessage.style.display = 'none'; // Oculta el elemento
+    }, 800); // Este tiempo debe coincidir con la duración de la transición en CSS
+	setTimeout(() => {
+        errorMessage.classList.remove('hide'); // Elimina la clase de ocultar para mostrar el mensaje
+    }, 800); // Retraso de 800 milisegundos
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.querySelector('.info__close');
+    closeButton.addEventListener('click', closeErrorMessage);
+});
+
+const ngrok = require("@ngrok/ngrok");
+
+(async function () {
+	const listener = await ngrok.forward({
+		addr: 5501,
+		authtoken_from_env: true,
+		request_header_add: ["is-ngrok:1", "country:${conn.geo.country_code}"],
+		request_header_remove: "referrer",
+	});
+
+	console.log(`Ingress established at: ${listener.url()}`);
+})();
---
+++ js/registro.js
@@ -0,0 +1,123 @@
+document.querySelector("#btnSendR").addEventListener('click', (event) => {
+    event.preventDefault(); // Previene el envío del formulario
+
+	document.getElementById('errorMessageName').style.display = 'none';
+    document.getElementById('errorMessageEmail').style.display = 'none';
+	document.getElementById('errorMessageEmailR').style.display = 'none';
+    document.getElementById('errorMessagePassword').style.display = 'none';
+
+    const nameR = document.querySelector('#nameR').value; 
+    const emailR = document.querySelector('#emailR').value; 
+    const passwordR = document.querySelector('#passwordR').value; 
+
+	let isValid = true;
+
+if (!validateName(nameR)) {
+    errorMessageName.style.display = 'flex';
+    isValid = false; // Cambia a false si hay un error en el nombre
+}
+
+if (!validateEmail(emailR)) {
+    errorMessageEmail.style.display = 'flex';
+    isValid = false; // Cambia a false si hay un error en el email
+}
+
+if (!validatePassword(passwordR)) {
+    errorMessagePassword.style.display = 'flex';    
+    isValid = false; // Cambia a false si hay un error en la contraseña
+}
+
+
+if (isValid) {
+
+    const data = { 
+        name: nameR, 
+        email: emailR, 
+        password: passwordR 
+    }; 
+
+loader.style.display = 'flex';
+
+    const URL = "http://localhost:4081/api/user/register";
+
+    fetch(URL, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data) 
+    })
+    .then(resp => resp.json())
+    .then(responseData => {
+		if (responseData.error === null) {
+        	loader.style.display = 'none';
+        const header = document.querySelector('.header');
+        header.style.display = 'block'; 
+		setTimeout(function() {
+    window.location.href = 'index.html';
+}, 2000);
+
+        } else {
+			loader.style.display = 'none';
+			errorMessageEmailR.style.display = 'flex';
+           // alert("Error en el registro: " + (responseData.error || "Error desconocido")); // Manejo de error más seguro
+        }
+    })
+    .catch(err => {
+        console.error(err);
+        alert("Ocurrió un error. Intenta nuevamente.");
+    });
+    }; 
+});
+
+
+function closeErrorMessage(elementId) {
+    const errorMessage = document.getElementById(elementId);
+    if (!errorMessage) return; 
+    errorMessage.classList.add('hide');
+    setTimeout(() => {
+        errorMessage.style.display = 'none'; 
+    }, 800);
+    setTimeout(() => {
+        errorMessage.classList.remove('hide'); 
+    }, 800);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.querySelector('.info__closeName');
+    closeButton.addEventListener('click', () => closeErrorMessage('errorMessageName'));
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.querySelector('.info__closeEmail');
+    closeButton.addEventListener('click', () => closeErrorMessage('errorMessageEmail'));
+});
+document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.querySelector('.info__closeEmailR');
+    closeButton.addEventListener('click', () => closeErrorMessage('errorMessageEmailR'));
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const closeButton = document.querySelector('.info__closePassword');
+    closeButton.addEventListener('click', () => closeErrorMessage('errorMessagePassword'));
+});
+
+
+function validateName(name) {
+    const nameRegex = /^[a-zA-Z\s]+$/; 
+    return name.length >= 6 && nameRegex.test(name);
+}
+
+function validarNombre(name) {
+    const regex = /^[a-zA-Z]{6}$/;
+    return regex.test(name);
+}
+
+function validateEmail(email) {
+    const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return email.trim() !== '' && emailRegex.test(email);
+}
+
+function validatePassword(password) {
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,30}$/;
+    return password.trim() !== '' && passwordRegex.test(password);
+}
